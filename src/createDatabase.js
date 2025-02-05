b743134c8bc380b1148946fa3a67f4691d493260
sanjeev8521 <pandeysanjeev2001@gmail.com>
2025-02-05 08:44:40 +0530

    second and last

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -3,7 +3,7 @@ const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb+srv://pandeysanjeev2001:Sanjeev2001@cluster0.47se6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DATABASE_URL = "mongodb+srv://pandeysanjeev2001:Sanjeev2001@cluster0.47se6.mongodb.net/";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
